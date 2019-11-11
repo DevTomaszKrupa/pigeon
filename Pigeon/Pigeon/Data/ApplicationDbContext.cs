b3e65ae413df5e6ffab8d958aa6b89ecc234d5e8
Tomasz Krupa <etomaszkrupa@gmail.com>
2019-11-11 13:53:10 +0100

    Added relations in context file

--- Pigeon/Pigeon/Data/ApplicationDbContext.cs
+++ Pigeon/Pigeon/Data/ApplicationDbContext.cs
@@ -5,6 +5,8 @@ namespace Pigeon.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ShoppingCard> ShoppingCards { get; set; }
         public DbSet<ShoppingCardProduct> ShoppingCardProducts { get; set; }
@@ -12,6 +14,19 @@ namespace Pigeon.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Product>()
+                   .HasKey(x => x.ProductId);
+
+            builder.Entity<Product>()
+                .HasMany(x => x.OrderProducts)
+                .WithOne(x => x.Product)
+                .HasForeignKey(x => x.ProductId);
         }
     }
 }
